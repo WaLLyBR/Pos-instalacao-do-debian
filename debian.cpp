847ec0f2b3acc06f9067a9bfa869d248f86e9b7f
Allythy <allythyrenan@gmail.com>
2016-12-11 22:21:58 -0300

    up

---
+++ debian.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+	 
+	 int opcao; char menu;
+	 
+	 cout << "::::: Essencial ::::: \n\n";
+	 
+	 cout <<"1- Atualizar o sistema\n" "2- Instalar o Ufw\n" "3- Instalar o vlc\n" "4- Instalar o Kazam\n" "5- Instalar o Telegram\n\n";
+	 cout <<"Escolhar uma opção: ";
+	 cin >> opcao;
+	
+	switch(opcao)
+	{
+		case 1:
+		system("sudo apt-get update && sudo apt-get upgrade");
+		cout <<"Deseja escolher outra opção [S/N]";
+		cin >> menu;
+		break;
+		
+		case 2:
+		system("sudo apt-get install ufw && sudo ufw enable");
+		cout <<"Deseja escolher outra opção [S/N]";
+		cin >> menu;
+		break;
+	
+		case 3:
+		system("sudo apt-get install vlc");
+		cout <<"Deseja escolher outra opção [S/N]";
+		cin >> menu;
+		break;
+
+		case 4:
+		system("sudo apt-get install kazam");
+		cout <<"Deseja escolher outra opção [S/N]";
+		cin >> menu;
+		break;
+	
+	}
+	
+}
